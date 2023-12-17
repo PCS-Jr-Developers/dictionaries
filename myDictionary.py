b0dcfe4d528feb27e7a4a70e3bedf021d599a7ae
Eric Doan <103901993+E-Doan@users.noreply.github.com>
2023-12-17 12:54:04 -0500

    Update myDictionary.py

--- myDictionary.py
+++ myDictionary.py
@@ -4,6 +4,10 @@ myDictionary2 = {'Fruit-1': "Apple", 'Fruit-2': "Orange"}
 
 myDictionary2['Fruit-3'] = 'Kiwi'
 
+myDictionary2.pop('Fruit-2')
+
 print(myDictionary2['Fruit-1'])
 
 print(myDictionary2['Fruit-3'])
+
+print(myDictionary2)
