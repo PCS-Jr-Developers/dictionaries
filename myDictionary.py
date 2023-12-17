0924df6865141aa9d6a587cd1926e4d486ca1073
Eric Doan <103901993+E-Doan@users.noreply.github.com>
2023-12-17 12:45:49 -0500

    Create myDictionary.py

---
+++ myDictionary.py
@@ -0,0 +1,9 @@
+myDictionary = {}
+
+myDictionary2 = {'Fruit-1': "Apple", 'Fruit-2': "Orange"}
+
+myDictionary2['Fruit-3'] = 'Kiwi'
+
+print(myDictionary2['Fruit-1'])
+
+print(myDictionary2['Fruit-3'])
